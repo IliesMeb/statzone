afb6d16e88b6848766c87079564891094a1d9f54
IliesMeb <ilies.mebarki@googlemail.com>
2021-07-08 17:40:58 +0200

    scraping setup done, but not finished

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-var express = require("express");
-var React = require('react');
+const express = require('express');
+const React = require('react');
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('tablestats.json');
 
-var app = express();
+const app = express();
+
+//write Headers
+//writeStream.write('1,2,3')
+
+request('https://fbref.com/de/fussballspieler/d6862409/Florian-Niederlechner', (error, response, html) => {
+    if(!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        // const allStats = $('#stats');
+        // const statsRow = allStats.find('td').each((index, element) => {
+        //     //console.log(index);
+        //     console.log($(element).text());
+        // });
+        // $('#stats').each((i,e) => { //DAS HIER BENUTZEN
+        //    const statsRrow = $(e).text();
+        //     // const attri = $(e).attr('data-stat'); //-> nochmal anschauen
+        //     // console.log(statsRrow);
+        // });
+    
+        //console.log(statsRow.html());
+        //console.log(allStats.text());
+        //console.log(statsRow);
+
+        $('#stats > td').each((i, el) => {
+            const row = $(el).text().replace(/\s\s+/g, '');
+            //write rows to CSV
+            writeStream.write(`${row}`);
+        });
+        console.log("scraping done");
+    }
+})
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
